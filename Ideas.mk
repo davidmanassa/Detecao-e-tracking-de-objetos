6a3712c87c049acf820d273cc34f64bb68a62daf
David <manassa97@hotmail.com>
2020-02-26 16:39:29 +0000

    Day -1
    Análise prévia do projecto

---
+++ Ideas.mk
@@ -0,0 +1,18 @@
+
+# Sistema de deteção e tracking de objetos em movimento em tempo real num feed de video recebido de um webcam
+
+- Raspberry 3
+- Webcam 
+
+Programação em/com
+- Python
+- Opencv
+- Yolo
+- Tensorflow
+
+# Stages
+
+Estudo das ferramentas utilizadas
+- Com documentação e testes
+
+
