724ce41e114077d0453d9b1c4385662355d825b2
David <manassa97@hotmail.com>
2020-03-21 18:07:13 +0000

    More things

--- Ideas.mk
+++ Ideas.mk
@@ -11,8 +11,32 @@ Programação em/com
 - Tensorflow
 
 # Stages
+Stage 1: 2 weeks
 
 Estudo das ferramentas utilizadas
 - Com documentação e testes
+
+Stage 2: 1 week
+
+Terminar Estado da Arte 
+Instalar Raspbian
+
+Stage 3:
+
+
+-------------------
+
+Arquiteturas de deteção de objetos
+
+Fazem em duas partes
+CNN
+R-CNN
+Fast R-CNN
+Faster R-CNN
+
+Fazem tudo de uma vez
+SSD
+YOLO
+
 
 
